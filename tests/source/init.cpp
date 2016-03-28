25452313d18b99ec435c7db8912c6e88ea63dbb1
dmred <dmredchenko@gmail.com>
2016-03-29 00:23:55 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -20,22 +20,3 @@ SCENARIO("Matrix init", "[init]") {
 		}
 	}
 }
-
-SCENARIO("Matrix +", "[addition]") {
-	matrix A = matrix(3, 3);
-	A.get_from_file("A3x3.txt");
-	matrix B = matrix(3, 3);
-	B.get_from_file("B3x3.txt");
-	matrix expected = matrix(3, 3);
-	expected.get_from_file("A+B.txt");
-	matrix result = A + B;
-	bool f=true;
-	for (int i = 1; i < 3; ++i) {
-		for (int j = 1; j < 3; ++j) {
-			if (result[i][j] != expected[i][j]) {
-				f= false;
-			}
-		}
-	}
-	REQUIRE(f == true);
-}
