2881493b34db2e3acb172225d04c93f2974913b6
dmred <dmredchenko@gmail.com>
2016-03-28 23:46:59 +0300

    Update matrix.h

--- include/matrix.h
+++ include/matrix.h
@@ -30,7 +30,7 @@ public:
 	friend matrix operator+(const matrix &matrix_1, const matrix &matrix_2);
 	friend matrix operator*(const matrix &matrix_1, const matrix &matrix_2);
 	int *operator[] (int i) const;
-	bool matrix operator ==(const matrix & m)
+	bool matrix operator ==(const matrix & m);
 	void get_sum_r(const matrix &matrix_1, const matrix &matrix_2, int s);
 	void get_multi_r(const matrix &matrix_1, const matrix &matrix_2, int m);
 	void create_memory();
